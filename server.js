8e7b75ae838fd3113f6e5be22e729e133dc99632
Eddy Calderon <edgarcalderon@Edgars-MacBook-Pro.local>
2021-09-25 21:19:33 -0600

    adding server.js and adding route folders

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require ("morgan");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
+userNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindandModify: false,
+});
+
+
+app.use(require("./contollers/apiRoutes"));
+app.use(require("./contollers/htmlRoutes"));
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
