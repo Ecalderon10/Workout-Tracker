4fb1a641cdfce1cef1792aafd7b4558638357e40
Eddy Calderon <edgarcalderon@Edgars-MacBook-Pro.local>
2021-09-26 14:58:11 -0600

    pushing updates to apiRoutes

---
+++ controllers/apiRoutes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const db = require ("../models");
+
+router.get("/workouts", (req,res) => {
+ const getData = db.Workout.find({})
+ .then((dbWorkout) => {
+    res.json(dbWorkout)
+ })
+ .catch((err) => {
+ res.status(500).json(err);
+ })
+});
+
+
+
+router.post("/workouts", ({body}, res) => {
+const postData = db.Workout.create(body)
+.then((dbWorkout) => {
+res.json(dbWorkout);
+})
+.catch((err) => {
+res.status(500).json(err);
+});
+})
+
+
+router.put("/workouts/:id", (req,res) => {
+    const updateData = db.Workout.findOneAndUpdate({_id: req.params.id}, 
+    {$inc: {totalDuration: req.body.duaration},
+    $push: { excercises: req.body},
+    },{ new: true })
+    .then((dbWorkout) => {
+    res.json(dbWorkout);
+    })
+    .catch((err) => {
+    res.status(500).json(err);
+    })
+})
+
+
+router.get("/workouts/range", async (req,res) => {
+ try {
+     const listOfWorkouts = await db.Workout.aggreagate([{
+        $addFields: {
+        totalDuration: {$sum: "$excercises.duration"},
+        totalWeight: {$sum: "$exercises.weight"},
+        totalSets: { $sum: "$excercises.sets"},
+        totalReps: { $sum: "$excercises.distance"},
+        totalDistance:{ $sum: "$excerises.distance"},
+        },
+     }])
+ }
+})
